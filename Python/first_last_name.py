8201dc161647ab7cd5aab49713334feb43201e43
xogoodnow <xo4ever9999@gmail.com>
2023-05-28 18:40:31 +0330

    Formatting print

---
+++ Python/first_last_name.py
@@ -0,0 +1,7 @@
+first_name = str(input())
+last_name = str(input())
+
+def greetings(first, last):
+    print(f"Hello {first} {last}! You just delved into Python")
+
+greetings(first_name, last_name)
